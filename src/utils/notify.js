2b594ef5825dabff777c395e3825fed3c329ff5a
devcjlozano <carlosjlp@gmail.com>
2022-08-10 16:39:32 +0200

    config optional param notify.js

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,10 +1,15 @@
 import { Notify } from "quasar";
 
 export const showNotify = (
-  configNotify = { message: "Mensaje", color: "secondary" }
+  configNotify = { message: "Mensaje", color: "secondary", position: "center" }
 ) => {
-  console.log(configNotify);
+  const configNotifyAux = {
+    message: configNotify.message ? configNotify.message : "Mensaje",
+    color: configNotify.color ? configNotify.color : "secondary",
+    position: configNotify.position ? configNotify.position : "center",
+    timeout: configNotify.timeout ? configNotify.timeout : 800,
+  };
   Notify.create({
-    ...configNotify,
+    ...configNotifyAux,
   });
 };
