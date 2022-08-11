7f690c6a5a298fd484d7d9a5452effb9b1ac48e6
devcjlozano <carlosjlp@gmail.com>
2022-08-11 14:05:50 +0200

    add styles in AddGame.vue and logic debounce searcher

--- src/pages/AddGame.vue
+++ src/pages/AddGame.vue
@@ -4,10 +4,22 @@
     <div class="add-game__searcher">
       <q-input
         outlined
+        bottom-slots
         v-model="gameName"
         label="Nombre"
         @update:model-value="searchGame"
-      />
+      >
+        <template v-slot:append>
+          <q-spinner-hourglass v-if="showSpinner" color="primary" />
+        </template>
+
+        <template v-slot:hint>
+          <span v-if="flagSearched && !listGames.length">
+            No se han encontrado juegos en el catálogo con el nombre introducido
+          </span>
+        </template>
+      </q-input>
+
       <div v-if="!flagSearched" class="add-game__searcher__info--inital">
         <q-icon name="o_search" size="10em" />
         <p class="p-subtitle text-center">
@@ -24,17 +36,42 @@
         v-if="flagSearched && !listGames.length"
         class="add-game__searcher__info--no-game"
       >
-        No se han encontrado juegos
+        <div>
+          <q-icon name="o_casino" class="icon_dice_left" size="8em" />
+          <q-icon name="o_casino" class="icon_dice_right" size="8em" />
+        </div>
+
+        <p class="p-subtitle text-center">
+          El nombre introducido no coincide con ningún juego del catálogo online
+        </p>
+        <p class="p-subtitle text-center">
+          Si no lo has hecho ya, puedes probar a ponerlo en inglés o bien añadir
+          tu juego manualmente aquí
+          <a href=""> Añadir juego manualmente </a>
+        </p>
       </div>
-      <div>
+      <div class="add-game__list-games">
         <q-list>
-          <q-item v-for="game in listGames" :key="game.id">
+          <q-item clickable v-for="game in listGames" :key="game.id">
             <q-item-section avatar>
               <q-avatar rounded>
-                <img :src="game.thumb_url" />
+                <img
+                  :src="game.thumb_url"
+                  @error="handlerError"
+                  alt="Front page boarg game"
+                />
               </q-avatar>
             </q-item-section>
-            <q-item-section>{{ game.name }}</q-item-section>
+
+            <q-item-section class="section-game-name">
+              {{ game.name }}
+            </q-item-section>
+
+            <q-item-section avatar>
+              <q-avatar rounded>
+                <q-icon name="arrow_right" />
+              </q-avatar>
+            </q-item-section>
           </q-item>
         </q-list>
       </div>
@@ -52,23 +89,42 @@ export default {
     const gameName = ref("");
     const flagSearched = ref(false);
     const listGames = ref([]);
+    const debounce = ref(null);
+    const showSpinner = ref(false);
 
     const searchGame = () => {
-      searchGameApi({ name: gameName.value })
-        .then(({ data }) => {
-          listGames.value = data.games;
-          flagSearched.value = true;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      showSpinner.value = true;
+      clearTimeout(debounce.value);
+
+      debounce.value = setTimeout(() => {
+        if (gameName.value !== "") {
+          searchGameApi({ name: gameName.value })
+            .then(({ data }) => {
+              listGames.value = data.games;
+              flagSearched.value = true;
+              showSpinner.value = false;
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        } else {
+          listGames.value = [];
+          showSpinner.value = false;
+        }
+      }, 400);
+    };
+
+    const handlerError = (e) => {
+      e.target.src = "";
     };
 
     return {
       gameName,
       flagSearched,
       listGames,
+      showSpinner,
       searchGame,
+      handlerError,
     };
   },
 };
@@ -84,5 +140,30 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+.add-game__searcher__info--no-game {
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.add-game__searcher__info--no-game p {
+  margin-top: 30px;
+}
+.add-game__list-games {
+  margin-top: 20px;
+}
+.q-item__section--side > .q-avatar {
+  font-size: 70px;
+}
+.section-game-name {
+  font-weight: bold;
+}
+.icon_dice_left {
+  transform: rotate(20deg);
+}
+.icon_dice_right {
+  transform: rotate(-20deg);
 }
 </style>
